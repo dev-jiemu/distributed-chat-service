5d57d2e4466eaedc620cd4d8347115f29d11f574
jiemu <rebianne03@naver.com>
2026-01-25 18:07:09 +0900

    - front 수정

--- src/main/java/com/example/chat/repository/UserRepository.java
+++ src/main/java/com/example/chat/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
-    Optional<User> findByClientIdentifier(String clientIdentifier);
     boolean existsByNickname(String nickname);
     Optional<User> findByEmail(String email);
 }
